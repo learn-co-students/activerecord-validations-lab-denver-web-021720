fe40186db813b2896441523563cbab52a75b12ec
joegelay <joegelay@gmail.com>
2020-03-30 10:52:04 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 
     def clickbait 
         clickbait_tags = [/Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/]
-        if clickbait_tags.none? { |reg| reg.match title }
+        if clickbait_tags.none? { |tag| tag.match title }
             errors.add(:title, "is not clickbait")
         end 
     end 
